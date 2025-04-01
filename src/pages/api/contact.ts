35f2364b55d78bfdd2275770377e0bd5ee931b78
afilazzola <alex.filazzola@outlook.com>
2025-03-31 21:22:07 -0400

    added email service

---
+++ src/pages/api/contact.ts
@@ -0,0 +1,48 @@
+import type { APIRoute } from 'astro'
+import nodemailer from 'nodemailer'
+
+export const POST: APIRoute = async ({ request }) => {
+  const data = await request.formData()
+  const name = data.get('name')
+  const email = data.get('email')
+  const message = data.get('message')
+
+  if (!name || !email || !message) {
+    return new Response(
+      JSON.stringify({ error: 'Missing required fields' }),
+      { status: 400 }
+    )
+  }
+
+  const transporter = nodemailer.createTransport({
+    host: 'email-smtp.ca-central-1.amazonaws.com',
+    port: 587,
+    secure: false, // use TLS
+    auth: {
+      user: import.meta.env.AWS_SES_USERNAME,
+      pass: import.meta.env.AWS_SES_PASSWORD,
+    },
+    tls: {
+      rejectUnauthorized: false, // <- allows self-signed certs
+    },
+  })
+  
+  try {
+    await transporter.sendMail({
+      from: import.meta.env.AWS_SES_EMAIL, 
+      to: import.meta.env.AWS_SES_EMAIL, 
+      subject: 'Amanda Liczner Website - Contact Form Submission',
+      text: message.toString(),
+    })
+
+    return new Response(JSON.stringify({ success: true }), {
+      status: 200,
+    })
+  } catch (err) {
+    console.error('Email error:', err)
+    return new Response(
+      JSON.stringify({ error: 'Failed to send email' }),
+      { status: 500 }
+    )
+  }
+}
