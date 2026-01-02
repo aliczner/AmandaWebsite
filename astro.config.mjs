e3d612627492d4769f04b1ca2e5219c4f74f3ca4
afilazzola <alex.filazzola@outlook.com>
2026-01-02 18:16:34 -0500

    Added SEO to pages where it was missing.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel/serverless'
 import astroExpressiveCode from 'astro-expressive-code'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://amandaliczner.ca',
   integrations: [
     tailwind(),
     astroExpressiveCode({
@@ -14,5 +16,6 @@ export default defineConfig({
         borderWidth: '0px',
       },
     }),
+    sitemap(),
   ],
 })
